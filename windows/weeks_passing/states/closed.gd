e8c9c3c304e36b97a1742e46df6395ccefe51c17
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-10-11 12:07:59 -0500

    Smoother closing of window

--- windows/weeks_passing/states/closed.gd
+++ windows/weeks_passing/states/closed.gd
@@ -1,11 +1,21 @@
 extends TextWindowState
 
 #
+#	Exported
+#
+
+@export_group("Required References")
+@export var text_reader: TextReader
+
+#
 #	State Functions
 #
 
 func state_enter(_message: Dictionary = {}) -> void:
 	text_window.visible = false
+	
+	if text_reader.get_state() != TextReader.State.Empty:
+		text_reader.cancel_reading()
 
 #
 #	Signals
